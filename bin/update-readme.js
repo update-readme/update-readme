e95ff140bf9a3aa7f05d631d78ee4a5ebfbfacf8
Will Hilton <wmhilton@gmail.com>
2017-03-19 22:11:32 -0400

    Simplify, simplify!

--- bin/update-readme.js
+++ bin/update-readme.js
@@ -1,43 +1,51 @@
 #!/usr/bin/env node
 'use strict'
-const fs = require('fs')
-const assert = require('assert')
-const subarg = require('subarg')
-const updateDoc = require('../lib/index')
+;(async function () {
+  try {
+    const fs = require('mz/fs')
+    const subarg = require('subarg')
+    const updateReadme = require('../lib/index')
 
-const argv = subarg(process.argv.slice(2), {
-  alias: {
-    plugin: ['p']
-  }
-})
-
-assert.ok(argv._[0], 'at least one argument (the filename to update) is required')
-
-let pluginOptions = argv.plugin
-if (!pluginOptions) {
-  pluginOptions = []
-}
-else if (typeof pluginOptions === 'string') {
-  pluginOptions = [pluginOptions]
-}
-let plugins = []
-for (let p of pluginOptions) {
-  if (typeof p === 'string') {
-    plugins.push({
-      module: 'update-readme-' + p,
-      options: {}
+    const argv = subarg(process.argv.slice(2), {
+      alias: {
+        plugin: ['p'],
+        filename: ['f']
+      },
+      default: {
+        filename: 'README.md'
+      }
     })
-  } else {
-    plugins.push({
-      module: 'update-readme-' + p._[0],
-      options: p
-    })
+
+    let pluginOptions = argv.plugin
+    if (!pluginOptions) {
+      pluginOptions = []
+    }
+    else if (typeof pluginOptions === 'string') {
+      pluginOptions = [pluginOptions]
+    }
+    let plugins = []
+    for (let p of pluginOptions) {
+      if (typeof p === 'string') {
+        plugins.push({
+          module: 'update-readme-' + p,
+          options: {}
+        })
+      } else {
+        plugins.push({
+          module: 'update-readme-' + p._[0],
+          options: p
+        })
+      }
+    }
+
+    let filebody = await fs.readFile(argv.filename, 'utf8')
+    let opts = {}
+    opts.plugins = plugins
+    opts.readme = filebody
+    filebody = await updateReadme(opts)
+    await fs.writeFile(argv.filename, filebody, 'utf8')
+    process.exit()
+  } catch (err) {
+    console.log(err.message)
   }
-}
-
-let opts = {}
-opts.plugins = plugins
-
-let command = updateDoc(opts)
-
-command(argv._[0]).then(process.exit)
+})()
